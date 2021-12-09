92ecd456e9b2a2b56377bebfbc3a54069a04aeca
Bobby-gh <knaihv@ymail.com>
2021-12-09 18:22:40 +0000

    new addition

--- index.php
+++ index.php
@@ -1,139 +1,136 @@
-<!DOCTYPE html>
-<html lang="en">
-<?php session_start() ?>
-<?php 
-	if(!isset($_SESSION['login_id']))
-	    header('location:login.php');
-    include 'db_connect.php';
-    ob_start();
-  if(!isset($_SESSION['system'])){
-
-    $system = $conn->query("SELECT * FROM system_settings")->fetch_array();
-    foreach($system as $k => $v){
-      $_SESSION['system'][$k] = $v;
+<?php
+/*********************************************************************************  
+ *  Sentrifugo is an open source human resource management system.
+ *  Copyright (C) 2014 Sapplica
+ *   
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Sentirfugo Support <support@sentrifugo.com>
+ ********************************************************************************/
+defined('CODEVERSION')|| define('CODEVERSION', '3.2');
+$filepath = 'install/index.php';
+if(file_exists($filepath))
+{
+header("Location: install/index.php");  
+}else
+{
+   
+try
+{
+// constants 
+    
+require_once 'public/constants.php';
+require_once 'public/site_constants.php';
+require_once 'public/email_constants.php';
+require_once 'public/emptabconfigure.php';
+require_once 'public/db_constants.php';
+require_once 'public/application_constants.php';
+require_once 'public/mail_settings_constants.php';
+require_once 'application/modules/default/library/sapp/Global.php';
+require_once 'public/text_constants.php';
+/* Query to fetch db version and then comparing with code version */
+try {
+	$mysqlPDO = new PDO('mysql:host='.SENTRIFUGO_HOST.';dbname='.SENTRIFUGO_DBNAME.'',SENTRIFUGO_USERNAME, SENTRIFUGO_PASSWORD,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));   
+    $date= gmdate("Y-m-d H:i:s");
+    $stmt =  $mysqlPDO->prepare("SHOW TABLES LIKE 'main_patches_version'");
+    $stmt->execute();
+    $count = $stmt->fetch();
+    if(!empty($count))
+    {
+    	$dbversion = '';
+		$dbid = '';
+		$codeversion = '';
+		$codeid = '';
+	    $stmt1 = $mysqlPDO->prepare("select * from main_patches_version where isactive=1 ");
+	    $stmt2 = $mysqlPDO->prepare("select * from main_patches_version where version ='".CODEVERSION."' and isactive=1  ");
+	    $stmt1->execute();
+	    $stmt2->execute();
+	    $dbdataArray = $stmt1->fetch();
+	    $codedataArray = $stmt2->fetch();
+	    if(!empty($dbdataArray))
+	    {
+	    	$dbversion = $dbdataArray['version'];
+	    	$dbid = $dbdataArray['id'];
+	    }
+	    
+	    if(!empty($codedataArray))	
+	    {
+	    	$codeversion = $codedataArray['version'];
+	    	$codeid = $codedataArray['id'];
+	    }else
+		{
+			$codeversion = CODEVERSION;
+		}
+    }else 
+    {
+    	header("Location: error.php?param=".sapp_Global::_encrypt('tbl')."");exit;
     }
-  }
-  ob_end_flush();
+}
+catch (PDOException $e)
+{
+	 header("Location: error.php?param=".sapp_Global::_encrypt('db')."");exit;
+}
+/**
+ *  If both codeversion and dbversion are equal then application will load.
+ *  Else it will be redirected to upgrade.php using form post. 
+*/
+	if(isset($dbid) && isset($codeid))
+		{
+			if($dbid == $codeid)
+			{
+				// Ensure library/ is on include_path
+				set_include_path(implode(PATH_SEPARATOR, array(
+				    realpath(APPLICATION_PATH . '/library'),
+				    get_include_path(),
+				)));
+				
+				
+				
+				/** Zend_Application */
+				require_once 'Zend/Application.php';
+				
+				
+				    // Create application, bootstrap, and run
+				    $application = new Zend_Application(
+				        APPLICATION_ENV,
+				        APPLICATION_PATH . '/configs/application.ini'
+				    );
+				    $application->bootstrap()
+				                ->run();
+			}else 
+			{?>
+				
+				<form name="upgrade" id="upgrade" action="upgrade.php" method="post" class="frm_install">
+				       <input type = 'hidden' value='<?php echo $codeversion;?>' name='codeversion' id='codeversion'>
+				       <input type = 'hidden' value='<?php echo $dbversion;?>' name='dbversion' id='dbversion'>
+				       <input type = 'hidden' value='<?php echo $dbid;?>' name='dbid' id='dbid'>
+				       <input type = 'hidden' value='<?php echo $codeid;?>' name='codeid' id='codeid'>
+				</form>
+				<script>
+					document.getElementById('upgrade').submit();
+				</script>
+			<?php }   
+		} else
+		{
 
-	include 'header.php' 
+			header("Location: error.php?param=".sapp_Global::_encrypt('db')."");exit;
+		}            
+}
+catch(Exception $e)
+{    
+
+	header("Location: error.php?param=".sapp_Global::_encrypt('error')."");exit;
+}
+}	
 ?>
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-<div class="wrapper">
-  <?php include 'topbar.php' ?>
-  <?php include 'sidebar.php' ?>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-  	 <div class="toast" id="alert_toast" role="alert" aria-live="assertive" aria-atomic="true">
-	    <div class="toast-body text-white">
-	    </div>
-	  </div>
-    <div id="toastsContainerTopRight" class="toasts-top-right fixed"></div>
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 bg-white"><?php echo $title ?></h1>
-          </div><!-- /.col -->
-
-        </div><!-- /.row -->
-            <hr class="border-primary">
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-         <?php 
-            $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-            if(!file_exists($page.".php")){
-                include '404.html';
-            }else{
-            include $page.'.php';
-
-            }
-          ?>
-      </div><!--/. container-fluid -->
-    </section>
-    <!-- /.content -->
-    <div class="modal fade" id="confirm_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
-      </div>
-      <div class="modal-body">
-        <div id="delete_content"></div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="uni_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-      </div>
-      <div class="modal-body">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="uni_modal_right" role='dialog'>
-    <div class="modal-dialog modal-full-height  modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span class="fa fa-arrow-right"></span>
-        </button>
-      </div>
-      <div class="modal-body">
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="viewer_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
-              <img src="" alt="">
-      </div>
-    </div>
-  </div>
-  </div>
-  <!-- /.content-wrapper -->
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!--<strong>Copyright &copy; 2020 <a href="https://www.sourcecodester.com/">sourcecodester.com</a>.</strong>-->
-    EMI'S performance Management System.
-    <div class="float-right d-none d-sm-inline-block">
-     
-    </div>
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-<!-- jQuery -->
-<!-- Bootstrap -->
-<?php include 'footer.php' ?>
-</body>
-</html>
